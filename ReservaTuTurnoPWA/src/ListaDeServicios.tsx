ee9c04c8ba1d407ddd6fc35cf5f8bfe321d9d0de
mmanna3 <mmmannna3@gmail.com>
2024-02-24 23:50:46 -0300

    Spinner creado

--- ReservaTuTurnoPWA/src/ListaDeServicios.tsx
+++ ReservaTuTurnoPWA/src/ListaDeServicios.tsx
@@ -1,31 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
 import CrearServicio from "./CrearServicio";
 import { Client, ServicioDTO } from "./api/clients";
-import { useQuery } from "@tanstack/react-query";
+import Spinner from "./utils/Spinner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const client = new Client(API_BASE_URL);
 
 const ListaDeServicios = () => {
-  const { data: servicios, error, isLoading } = useQuery(
-    { 
-      queryKey: ['servicios'], 
-      queryFn: async () => await client.servicioAll() 
-    });
+  const {
+    data: servicios,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ["servicios"],
+    queryFn: async () => await client.servicioAll(),
+  });
 
   return (
-    <>      
-      <CrearServicio onCreate={() => null}/>
+    <>
+      <CrearServicio onCreate={() => null} />
       <h2 className="text-2xl mb-4 mt-16">Servicios</h2>
-      {isLoading? <div>Cargando..</div> : null}
-      {error? <div>Error: {error.message}</div> : null}
-      
-      {servicios ? 
-        servicios.map((s: ServicioDTO) => (
-        <div key={s.id} className="border rounded-lg p-8 mb-2">
-          <p className="text-lg font-semibold mb-2">{s.nombre}</p>
-          <p>{s.descripcion}</p>
-        </div>
-      )): null}
+      {isLoading ? <Spinner tamanio="meh" /> : null}
+      {error ? <div>Error: {error.message}</div> : null}
+
+      {servicios
+        ? servicios.map((s: ServicioDTO) => (
+            <div key={s.id} className="border rounded-lg p-8 mb-2">
+              <p className="text-lg font-semibold mb-2">{s.nombre}</p>
+              <p>{s.descripcion}</p>
+            </div>
+          ))
+        : null}
     </>
   );
 };
