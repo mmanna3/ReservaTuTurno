58f9348c9c9c86d455375f893b00b7b6f0b10ce2
mmanna3 <mmmannna3@gmail.com>
2024-02-25 03:31:37 -0300

    React router instalado y funcionando

--- ReservaTuTurnoPWA/src/main.tsx
+++ ReservaTuTurnoPWA/src/main.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import ListaDeServicios from "./ListaDeServicios.tsx";
+import "./index.css";
+import Home from "./routes/Home.tsx";
+import Root from "./routes/Root.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "servicios",
+        element: <ListaDeServicios />,
+      },
+    ],
+  },
+]);
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
