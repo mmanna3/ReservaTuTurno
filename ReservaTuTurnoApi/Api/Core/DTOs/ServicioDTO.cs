1627671ce8cbcb914c3908e9b65ed6607a48401f
mmanna3 <mmmannna3@gmail.com>
2024-02-26 22:43:06 -0300

    Instala react-hook-form y lo hace andar

--- ReservaTuTurnoApi/Api/Core/DTOs/ServicioDTO.cs
+++ ReservaTuTurnoApi/Api/Core/DTOs/ServicioDTO.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Api.Core.DTOs;
 
 public class ServicioDTO : DTO
 {
+    [Required]
     public string Nombre { get; set; }
     public string? Descripcion { get; set; }
+    public int? PrecioPorDefecto { get; set; }
+    public int? DuracionDelTurnoPorDefectoEnMinutos { get; set; }
 }
