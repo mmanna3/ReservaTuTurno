1c1bcd00c91dc24b2da6e1edbad6c393f086838c
mmanna3 <mmmannna3@gmail.com>
2024-03-07 23:44:46 -0300

    La lista es scrollable

--- ReservaTuTurnoPWA/src/pages/servicio/ListaDeServiciosPage.tsx
+++ ReservaTuTurnoPWA/src/pages/servicio/ListaDeServiciosPage.tsx
@@ -30,18 +30,18 @@ const ListaDeServicios = () => {
       {isLoading ? <Spinner tamanio="meh" /> : null}
       {error ? <div>Error: {error.message}</div> : null}
 
-      <div className="overflow-auto">
+      <div className="h-screen overflow-auto">
         {servicios
           ? servicios.map((s: ServicioDTO) => (
               <div
                 key={s.id}
-                className="border rounded-lg w-96 p-8 mb-2 text-zinc-500"
+                className="border rounded-lg w-64 p-8 mb-2 text-zinc-500"
               >
-                <p className="text-left text-lg font-semibold  mb-2 text-zinc-800">
+                <p className="text-left text-base font-semibold mb-2 text-zinc-800">
                   {s.nombre}
                 </p>
-                <p className="text-left">{s.descripcion}</p>
-                <div className="flex justify-between mt-5 w-full">
+                <p className="text-left text-sm">{s.descripcion}</p>
+                <div className="flex text-sm justify-between mt-5 w-full">
                   <p>{s.precioPorDefecto}$</p>
                   <p>{s.duracionDelTurnoPorDefectoEnMinutos}min</p>
                 </div>
