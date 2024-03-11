5cd9b8840c6f634c535a25079f71ef69c52e46aa
mmanna3 <mmmannna3@gmail.com>
2024-03-10 21:01:16 -0300

    Muestra bien el error en el input

--- ReservaTuTurnoPWA/src/components/Input.tsx
+++ ReservaTuTurnoPWA/src/components/Input.tsx
@@ -26,20 +26,26 @@ const Input = <T extends FieldValues>({
     <div className="relative z-0 px-2 w-full group text-left">
       <label
         htmlFor={props.name as string}
-        className="text-[11px] bg-white left-1 relative px-1 top-2 w-auto group-focus-within:text-blue-500 group-focus-within:font-bold"
+        className={`text-[11px] bg-white left-1 relative px-1 top-2 w-auto group-focus-within:text-blue-500 group-focus-within:font-bold ${
+          errors[props.name as string] && "text-red-600"
+        }`}
       >
         {props.label}
       </label>
       <input
         {...props}
-        //className="block text-slate-800 shadow appearance-none border rounded w-56 h-12 py-2 px-3 leading-tight focus:outline-none focus:shadow-outline my-2"
-        className="h-10 text-10 border py-55-rem border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className={`h-10 text-10 border py-55-rem border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400 ${
+          errors[props.name as string] &&
+          "border-red-600 focus:border-red-600 focus:ring-red-600"
+        }`}
         {...register(props.name as string, { required: required })}
       />
 
       {errors[props.name as string] &&
         errors[props.name]?.type === "required" && (
-          <span>Este campo es requerido</span>
+          <span className="text-[11px] text-red-600">
+            Este campo es requerido
+          </span>
         )}
     </div>
   );
