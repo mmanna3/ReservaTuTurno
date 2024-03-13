be19ef7c0e47ee41c9a116d336a335c7df755a0c
mmanna3 <mmmannna3@gmail.com>
2024-03-12 23:39:57 -0300

    Instala la fuente Roboto

--- ReservaTuTurnoPWA/tailwind.config.js
+++ ReservaTuTurnoPWA/tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
