f515e9b711e3376c52283d1ee6137a0022ba3fa3
mmanna3 <mmmannna3@gmail.com>
2024-03-14 03:10:55 -0300

    Configura link de algunos botones del footer

--- ReservaTuTurnoPWA/src/components/Footer.tsx
+++ ReservaTuTurnoPWA/src/components/Footer.tsx
@@ -1,12 +1,17 @@
+import { Link } from "react-router-dom";
 import { Calendar, Home, Settings, UserCircle } from "solar-icon-set";
 
 const Footer = () => {
   return (
     <div className="flex h-20 items-center justify-around bg-[#F9F9F9]">
-      <Home size={24} color="black" />
+      <Link to="/home">
+        <Home size={24} color="black" />
+      </Link>
       <Calendar size={24} color="black" iconStyle="Outline" />
       <UserCircle size={24} color="black" />
-      <Settings size={24} color="black" />
+      <Link to="/servicios">
+        <Settings size={24} color="black" />
+      </Link>
     </div>
   );
 };
