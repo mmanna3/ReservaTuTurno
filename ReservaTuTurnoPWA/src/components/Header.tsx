b1990a1c6c4f67953b3e8941ee289ab058a02256
mmanna3 <mmmannna3@gmail.com>
2024-03-14 03:08:23 -0300

    Optimiza imports

--- ReservaTuTurnoPWA/src/components/Header.tsx
+++ ReservaTuTurnoPWA/src/components/Header.tsx
@@ -1,9 +1,9 @@
-import * as SolarIconSet from "solar-icon-set";
+import { AltArrowLeft } from "solar-icon-set";
 
 export function Header() {
   return (
     <div className="mb-1 grid h-16 content-center pl-10">
-      <SolarIconSet.AltArrowLeft size={24} color="black" />
+      <AltArrowLeft size={24} color="black" />
     </div>
   );
 }
