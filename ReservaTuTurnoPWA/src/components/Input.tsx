25614aa62b6376c124c9211a65b2e5f7ffece67c
mmanna3 <mmmannna3@gmail.com>
2024-03-16 21:06:39 -0300

    Prueba para quitar borde azul en mobile al hacer foco en input

--- ReservaTuTurnoPWA/src/components/Input.tsx
+++ ReservaTuTurnoPWA/src/components/Input.tsx
@@ -26,7 +26,7 @@ const Input = <T extends FieldValues>({
     <div className="group relative z-0 -my-[0.3rem] w-full text-left">
       <label
         htmlFor={props.name as string}
-        className={`group-focus-within:text-verde group-focus-visible:text-verde text-gris relative left-4 top-[1.8rem] w-auto bg-transparent px-1 text-[12px] group-focus-within:font-bold ${
+        className={`relative left-4 top-[1.8rem] w-auto bg-transparent px-1 text-[12px] text-gris group-focus-within:font-bold group-focus-within:text-verde group-focus-visible:text-verde ${
           errors[props.name as string] && "!text-rojo"
         }`}
       >
@@ -35,7 +35,7 @@ const Input = <T extends FieldValues>({
 
       <input
         {...props}
-        className={`text-10 py-55-rem bg-grisclarito focus:border-verde focus:outline-verde focus:ring-verde focus-visible:text-verde focus-visible:outline-verde text-negro placeholder-grisclaro block h-16 w-full rounded-xl p-2.5 pl-5 pt-7 text-base shadow-none ${
+        className={`text-10 py-55-rem block h-16 w-full rounded-xl bg-grisclarito p-2.5 pl-5 pt-7 text-base text-negro placeholder-grisclaro !shadow-none focus:!border-verde focus:!outline-verde focus:!ring-verde focus-visible:text-verde focus-visible:!outline-verde ${
           errors[props.name as string] &&
           "border-rojo focus:border-rojo focus:ring-rojo focus-visible:outline-rojo"
         }`}
@@ -43,7 +43,7 @@ const Input = <T extends FieldValues>({
       />
       {errors[props.name as string] &&
         errors[props.name]?.type === "required" && (
-          <span className="text-rojo pl-3 text-sm">
+          <span className="pl-3 text-sm text-rojo">
             Este campo es requerido
           </span>
         )}
