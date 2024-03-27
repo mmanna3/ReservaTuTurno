e9eb6850443bc7ff452f6de670e8b618015526a3
mmanna3 <mmmannna3@gmail.com>
2024-03-27 03:45:28 -0300

    Elimina comentarios

--- ReservaTuTurnoPWA/src/pages/HomePage.tsx
+++ ReservaTuTurnoPWA/src/pages/HomePage.tsx
@@ -11,27 +11,6 @@ const Home = () => {
         </h1>
         <p className="text-sm">Gestioná tus turnos</p>
       </div>
-      {/* <div>
-        <Link className="text-lg text-slate-50" to="/servicios">
-          <button className="mt-2 h-24 w-64 rounded-xl bg-rosa text-slate-50">
-            Servicios
-          </button>
-        </Link>
-      </div>
-      <div>
-        <Link className="text-lg text-slate-50" to="/profesionales">
-          <button className="mt-8 h-24 w-64 rounded-xl bg-rosa text-slate-50">
-            Profesionales
-          </button>
-        </Link>
-      </div>
-      <div>
-        <Link className="text-lg text-slate-50" to="/servicios/categoria/crear">
-          <button className="mt-8 h-24 w-64 rounded-xl bg-rosa text-slate-50">
-            Crear categoría de servicio
-          </button>
-        </Link>
-      </div> */}
     </ContenedorCentradoConMargenes>
   );
 };
