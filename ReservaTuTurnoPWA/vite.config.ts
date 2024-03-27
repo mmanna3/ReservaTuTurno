3833e2989c96db44099d2090b97d1efb72c46ac5
mmanna3 <mmmannna3@gmail.com>
2024-03-27 03:54:30 -0300

    Prueba config de VitePWA (hay problemas con el SW)

--- ReservaTuTurnoPWA/vite.config.ts
+++ ReservaTuTurnoPWA/vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      injectRegister: "script",
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
