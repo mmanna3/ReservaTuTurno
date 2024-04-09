6ee25ae2a0e92e968688807bdb1802f3774dd475
mmanna3 <mmmannna3@gmail.com>
2024-04-08 23:57:52 -0300

    Primer test Agendas.Obtener con Profesional y Servicio

--- ReservaTuTurnoApi/Api/Core/Servicios/AgendaCore.cs
+++ ReservaTuTurnoApi/Api/Core/Servicios/AgendaCore.cs
@@ -14,8 +14,8 @@ public class AgendaCore : ABMCore<IAgendaRepo, Agenda, AgendaDTO>, IAgendaCore
         _repo = repo;
     }
 
-    public IList<Agenda> Obtener(Profesional? profesional, Servicio? servicio)
+    public async Task<IList<Agenda>> Obtener(Profesional? profesional, Servicio? servicio)
     {
-        return _repo.Obtener(profesional, servicio);
+        return await _repo.Obtener(profesional, servicio);
     }
 }
--- ReservaTuTurnoApi/Api/Persistencia/Repositorios/AgendaRepo.cs
+++ ReservaTuTurnoApi/Api/Persistencia/Repositorios/AgendaRepo.cs
@@ -13,19 +13,19 @@ public class AgendaRepo : RepositorioABM<Agenda>, IAgendaRepo
     }
 
     // ReSharper disable once ConvertIfStatementToSwitchStatement
-    public IList<Agenda> Obtener(Profesional? profesional, Servicio? servicio)
+    public async Task<List<Agenda>> Obtener(Profesional? profesional, Servicio? servicio)
     {
         if (servicio == null && profesional == null)
             throw new Exception("Tiene que haber profesional o servicio");
         
         if (servicio == null)
-            return Context.Agendas.Where(x => x.ProfesionalId == profesional!.Id).ToList();
+            return await Context.Agendas.Where(x => x.ProfesionalId == profesional!.Id).ToListAsync();
         
         if (profesional == null)
-            return Context.Agendas.Where(x => x.Servicios.Select(s => s.ServicioDelProfesional.ServicioId).Contains(servicio.Id)).ToList();
+            return await Context.Agendas.Where(x => x.Servicios.Select(s => s.ServicioDelProfesional.ServicioId).Contains(servicio.Id)).ToListAsync();
         
         var servicioDelProfesional = Context.Set<ServiciosDelProfesional>().Where(x => x.ProfesionalId == profesional.Id && x.ServicioId == servicio.Id);
-        return servicioDelProfesional.SelectMany(x => x.Agendas.Select(a => a.Agenda)).ToList();
+        return await servicioDelProfesional.SelectMany(x => x.Agendas.Select(a => a.Agenda)).ToListAsync();
     }
     
     protected override IQueryable<Agenda> Set()
