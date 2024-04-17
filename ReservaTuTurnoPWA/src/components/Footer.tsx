4b0f964798e093e2133621ff2461c8eea8de0a0c
mmanna3 <mmmannna3@gmail.com>
2024-04-17 03:58:05 -0300

    Agranda tamaño de botones en el footer

--- ReservaTuTurnoPWA/src/components/Footer.tsx
+++ ReservaTuTurnoPWA/src/components/Footer.tsx
@@ -1,21 +1,37 @@
 import { HomeIcon, CalendarIcon, UserCircleIcon, Cog6ToothIcon } from "@heroicons/react/24/outline";
+import { SVGAttributes } from "react";
 import { Link } from "react-router-dom";
+
+interface FooterItemProps {
+  linkTo: string;
+  icono: string;  
+}
+
+const iconos: Record<string, React.FC> = {
+  home: HomeIcon,  
+  turnos: CalendarIcon,
+  clientes: UserCircleIcon,
+  config: Cog6ToothIcon,
+};
+
+const FooterItem = (props: FooterItemProps) => {  
+
+  const Icono = iconos[props.icono] as unknown as React.FC<{ className: string }>;
+  
+  return (
+    <Link to={props.linkTo} className="w-full h-full flex items-center justify-center">
+        <Icono className="h-6 w-6 text-negro" />
+      </Link>
+  )
+}
 
 const Footer = () => {
   return (
-    <div className="flex h-20 items-center justify-around bg-[#FBF5F5]">
-      <Link to="/home">
-        <HomeIcon className="h-6 w-6 text-negro" />
-      </Link>
-      <Link to="/turnos">
-        <CalendarIcon className="h-6 w-6 text-negro" />
-      </Link>
-      <Link to="/clientes">
-        <UserCircleIcon className="h-6 w-6 text-negro" />
-      </Link>
-      <Link to="/configuracion">
-        <Cog6ToothIcon className="h-6 w-6 text-negro" />
-      </Link>
+    <div className="flex h-20 items-center bg-[#FBF5F5]">
+      <FooterItem linkTo="/home" icono="home" />
+      <FooterItem linkTo="/turnos" icono="turnos" />
+      <FooterItem linkTo="/clientes" icono="clientes" />
+      <FooterItem linkTo="/configuracion" icono="config" />
     </div>
   );
 };
