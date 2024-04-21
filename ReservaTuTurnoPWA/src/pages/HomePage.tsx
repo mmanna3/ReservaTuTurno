1798eca4c30e39bf811532732494e971a31fdb91
mmanna3 <mmmannna3@gmail.com>
2024-04-21 18:38:41 -0300

    Revierte cambios en home

--- ReservaTuTurnoPWA/src/pages/HomePage.tsx
+++ ReservaTuTurnoPWA/src/pages/HomePage.tsx
@@ -1,17 +1,16 @@
 import ContenedorCentradoConMargenes from "../components/ContenedorCentradoConMargenes";
-import ModalPicker from "../components/TimePicker";
+import FrambuesaIcono from "./../assets/icono-frambuesa.svg";
 
 const Home = () => {
   return (
     <ContenedorCentradoConMargenes>
-      <ModalPicker />
-      {/* <div className="flex h-full flex-col items-center justify-center">
+      <div className="flex h-full flex-col items-center justify-center">
         <img src={FrambuesaIcono} alt="Frambuesa" width="60" height="60" />
         <h1 className="mb-2 mt-4 text-3xl font-bold tracking-wide text-rosaoscuro">
           Frambuesa
         </h1>
         <p className="text-sm">Gestioná tus turnos</p>
-      </div> */}
+      </div>
     </ContenedorCentradoConMargenes>
   );
 };
