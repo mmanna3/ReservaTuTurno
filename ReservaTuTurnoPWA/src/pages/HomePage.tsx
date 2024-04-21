60cdbd2ecd3c8227490bfe910124223273a94f97
mmanna3 <mmmannna3@gmail.com>
2024-04-21 18:10:07 -0300

    Instala y prueba react-mobile-picker

--- ReservaTuTurnoPWA/src/pages/HomePage.tsx
+++ ReservaTuTurnoPWA/src/pages/HomePage.tsx
@@ -1,16 +1,29 @@
+// const selections = {
+//   title: ["Mr.", "Mrs.", "Ms.", "Dr."],
+//   firstName: ["John", "Micheal", "Elizabeth"],
+//   lastName: ["Lennon", "Jackson", "Jordan", "Legend", "Taylor"],
+// };
+
 import ContenedorCentradoConMargenes from "../components/ContenedorCentradoConMargenes";
-import FrambuesaIcono from "./../assets/icono-frambuesa.svg";
+import ModalPicker from "./ModalPicker";
 
 const Home = () => {
+  //   const [pickerValue, setPickerValue] = useState({
+  //     title: "Mr.",
+  //     firstName: "Micheal",
+  //     lastName: "Jordan",
+  //   });
+
   return (
     <ContenedorCentradoConMargenes>
-      <div className="flex h-full flex-col items-center justify-center">
+      <ModalPicker />
+      {/* <div className="flex h-full flex-col items-center justify-center">
         <img src={FrambuesaIcono} alt="Frambuesa" width="60" height="60" />
         <h1 className="mb-2 mt-4 text-3xl font-bold tracking-wide text-rosaoscuro">
           Frambuesa
         </h1>
         <p className="text-sm">Gestioná tus turnos</p>
-      </div>
+      </div> */}
     </ContenedorCentradoConMargenes>
   );
 };
