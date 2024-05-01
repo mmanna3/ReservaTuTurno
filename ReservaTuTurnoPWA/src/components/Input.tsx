763945468899b4064d3c10cde4b84af013473926
mmanna3 <mmmannna3@gmail.com>
2024-05-01 02:25:07 -0300

    Desinstala addon onboarding de storybook y refactor input

--- ReservaTuTurnoPWA/src/components/Input.tsx
+++ ReservaTuTurnoPWA/src/components/Input.tsx
@@ -14,24 +14,26 @@ interface InputProps
 }
 
 const Input = (props: InputProps) => {
+  const { label, esRequerido, hayError, name, ...rest } = props;
+
   return (
     <div className="group relative z-0 -my-[0.3rem] w-full text-left">
       <label
-        htmlFor={props.name}
+        htmlFor={name}
         className={cx(
-          props.hayError && "!text-rojo",
+          hayError && "!text-rojo",
           "relative left-4 top-[1.8rem] w-auto bg-transparent px-1 text-[12px] text-gris group-focus-within:font-bold group-focus-within:text-verde group-focus-visible:text-verde",
         )}
       >
-        {props.label} {props.esRequerido ? "*" : null}
+        {label} {esRequerido ? "*" : null}
       </label>
 
       <input
         // Acá filtrar solo las que son props del input
-        {...props}
-        data-testid={`input-${props.name}`}
+        {...rest}
+        data-testid={`input-${name}`}
         className={cx(
-          props.hayError
+          hayError
             ? "border-rojo focus:border-rojo focus:ring-rojo focus-visible:text-rojo focus-visible:outline-rojo"
             : "focus:border-verde focus:outline-verde focus:ring-verde focus-visible:text-verde focus-visible:outline-verde",
           "text-10 py-55-rem block h-16 w-full rounded-xl bg-grisclarito p-2.5 pl-5 pt-7 text-base text-negro placeholder-grisclaro !shadow-none",
