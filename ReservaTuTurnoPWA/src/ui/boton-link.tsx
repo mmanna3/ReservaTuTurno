b8ff3b29800ff74ec88119f08e13b9425fb02be5
mmanna3 <mmmannna3@gmail.com>
2024-05-08 01:52:12 -0300

    Estética: espaciado entre letras botones

--- ReservaTuTurnoPWA/src/ui/boton-link.tsx
+++ ReservaTuTurnoPWA/src/ui/boton-link.tsx
@@ -8,7 +8,7 @@ interface IProps {
 export function BotonLink(props: IProps) {
   return (
     <Link
-      className="my-6 flex w-full items-center justify-center rounded-xl bg-rosa py-5 text-lg text-blanco shadow-md"
+      className="my-6 flex w-full items-center justify-center rounded-xl bg-rosa py-5 text-lg tracking-wider text-blanco shadow-lg"
       to={props.link}
     >
       {props.texto}
