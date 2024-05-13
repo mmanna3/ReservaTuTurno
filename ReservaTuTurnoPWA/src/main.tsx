77a991502412357ead72749c49a1b8979c77b8b6
mmanna3 <mmmannna3@gmail.com>
2024-05-13 12:28:38 -0300

    Refactor: renombra todo lo que era page a home

--- ReservaTuTurnoPWA/src/main.tsx
+++ ReservaTuTurnoPWA/src/main.tsx
@@ -4,21 +4,21 @@ import "react-day-picker/dist/style.css";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import Agendas from "./pages/agendas/page.tsx";
+import Agendas from "./pages/agendas/home.tsx";
 import CrearCategoriaDeServicio from "./pages/categorias-de-servicio/crear.tsx";
-import ClientesPage from "./pages/clientes/page.tsx";
-import Configuracion from "./pages/configuracion/page.tsx";
+import ClientesPage from "./pages/clientes/home.tsx";
+import Configuracion from "./pages/configuracion/home.tsx";
 import ErrorPage from "./pages/error-page.tsx";
 import Home from "./pages/home-page.tsx";
 import CrearProfesionalPage from "./pages/profesionales/crear.tsx";
-import ListaDeProfesionales from "./pages/profesionales/lista.tsx";
+import ListaDeProfesionales from "./pages/profesionales/home.tsx";
 import Root from "./pages/root-page.tsx";
 import Equipo from "./pages/servicios/componentes/equipo.tsx";
 import CrearServicio from "./pages/servicios/crear.tsx";
 import EditarServicio from "./pages/servicios/editar.tsx";
-import ListaDeServicios from "./pages/servicios/lista.tsx";
+import ListaDeServicios from "./pages/servicios/home.tsx";
 import CrearTurno from "./pages/turnos/crear.tsx";
-import Turnos from "./pages/turnos/page.tsx";
+import Turnos from "./pages/turnos/home.tsx";
 import ErrorBoundary from "./ui/error-boundary.tsx";
 
 const router = createBrowserRouter([
