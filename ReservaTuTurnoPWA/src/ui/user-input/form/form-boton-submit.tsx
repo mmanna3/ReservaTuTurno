936556648e1f32b1f133ddd6f4782b1908d718d5
mmanna3 <mmmannna3@gmail.com>
2024-05-15 22:59:49 -0300

    Estética: botón onSubmit deshabilitado más clarito

--- ReservaTuTurnoPWA/src/ui/user-input/form/form-boton-submit.tsx
+++ ReservaTuTurnoPWA/src/ui/user-input/form/form-boton-submit.tsx
@@ -7,7 +7,7 @@ export function BotonSubmit(props: IBotonSubmit) {
   return (
     <input
       type="submit"
-      className="mt-8 w-full cursor-pointer rounded-xl bg-rosa py-5 text-lg font-medium tracking-wider text-blanco shadow-lg disabled:bg-gris"
+      className="mt-8 w-full cursor-pointer rounded-xl bg-rosa py-5 text-lg font-medium tracking-wider text-blanco shadow-lg disabled:bg-grisclaro"
       value={props.texto || "Crear"}
       disabled={props.estaDeshabilitado}
     />
